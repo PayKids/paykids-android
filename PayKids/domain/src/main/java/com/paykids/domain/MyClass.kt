b744b70de0b06aed035d25247bb96e6ac4bd4886
HamBeomJoon <hbj0802@naver.com>
2024-11-17 23:45:40 +0900

    setting: data, domain, presentation 모듈 생성

---
+++ PayKids/domain/src/main/java/com/paykids/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.paykids.domain
+
+class MyClass {
+}
--- PayKids/presentation/build.gradle.kts
+++ PayKids/presentation/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.paykids.paykids"
+    namespace = "com.paykids.presentation"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.paykids.paykids"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -37,15 +34,11 @@ android {
 
 dependencies {
 
-    implementation(project(":data"))
     implementation(project(":domain"))
-    implementation(project(":presentation"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
