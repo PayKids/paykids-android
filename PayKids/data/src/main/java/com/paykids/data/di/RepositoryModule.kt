058d442ee8741b15fff246966bc2ece5ac8d1773
HamBeomJoon <hbj0802@naver.com>
2024-11-20 16:30:38 +0900

    style: import 정리

--- PayKids/data/src/main/java/com/paykids/data/di/RepositoryModule.kt
+++ PayKids/data/src/main/java/com/paykids/data/di/RepositoryModule.kt
@@ -1,10 +1,8 @@
 package com.paykids.data.di
 
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
