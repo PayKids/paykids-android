487147224d2c6c26283ac4ffbf1f5384b607595d
HamBeomJoon <hbj0802@naver.com>
2024-11-20 16:30:01 +0900

    feat: GlobalApplication LoggerUtil 추가

--- PayKids/app/src/main/java/com/paykids/paykids/GlobalApplication.kt
+++ PayKids/app/src/main/java/com/paykids/paykids/GlobalApplication.kt
@@ -1,11 +1,17 @@
 package com.paykids.paykids
 
 import android.app.Application
+import com.paykids.util.LoggerUtil
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        printStartingLog()
     }
+
+    private fun printStartingLog() =
+        LoggerUtil.v(this.getString(R.string.app_name) + " Start!")
 }
