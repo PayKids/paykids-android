3a061f61d2d8a0b2869dce6800f8b090dae9bf48
HamBeomJoon <hbj0802@naver.com>
2024-11-22 13:41:35 +0900

    style: remove unused import

--- PayKids/app/src/main/java/com/paykids/paykids/MainActivity.kt
+++ PayKids/app/src/main/java/com/paykids/paykids/MainActivity.kt
@@ -3,8 +3,6 @@ package com.paykids.paykids
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.paykids.navigation.Screen
-import com.paykids.paykids.databinding.ActivityMainBinding
 import com.paykids.presentation.view.splash.SplashActivity
 import dagger.hilt.android.AndroidEntryPoint
 
--- PayKids/presentation/src/main/java/com/paykids/presentation/view/home/HomeActivity.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/home/HomeActivity.kt
@@ -1,9 +1,7 @@
 package com.paykids.presentation.view.home
 
-import android.content.Intent
 import com.paykids.presentation.base.BaseActivity
 import com.paykids.presentation.databinding.ActivityHomeBinding
-import com.paykids.presentation.databinding.ActivityStartBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
