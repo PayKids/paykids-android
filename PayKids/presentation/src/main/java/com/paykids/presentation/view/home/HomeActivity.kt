998c0db517176897348b7ee73f5edfefcf9c6c7f
HamBeomJoon <hbj0802@naver.com>
2024-11-22 13:39:07 +0900

    feat: activity 임시 화면 추가

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/home/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.paykids.presentation.view.home
+
+import android.content.Intent
+import com.paykids.presentation.base.BaseActivity
+import com.paykids.presentation.databinding.ActivityHomeBinding
+import com.paykids.presentation.databinding.ActivityStartBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+    override fun initView() {
+    }
+
+    override fun setObserver() {
+        super.setObserver()
+    }
+}
