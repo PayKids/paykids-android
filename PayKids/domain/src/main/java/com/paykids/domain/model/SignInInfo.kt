6ba249cd2222d3840d4d05f22adfd4abc27ffd5a
HamBeomJoon <hbj0802@naver.com>
2024-11-27 10:34:47 +0900

    refactor: SignInInfo 데이터 클래스 변수 추가

--- PayKids/domain/src/main/java/com/paykids/domain/model/SignInInfo.kt
+++ PayKids/domain/src/main/java/com/paykids/domain/model/SignInInfo.kt
@@ -6,5 +6,6 @@ data class SignInInfo(
     val idToken: String,
     val provider: AuthProvider,
     val email: String? = null,
-    val name: String? = null
+    val name: String? = null,
+    val isRegister: Boolean? = false,
 )
