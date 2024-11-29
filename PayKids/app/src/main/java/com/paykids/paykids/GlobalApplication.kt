d3969051e9c0fee175e90d07550b9547217478e2
HamBeomJoon <hbj0802@naver.com>
2024-11-29 13:38:27 +0900

    refactor: kakaoSdk init 위치 변경

--- PayKids/app/src/main/java/com/paykids/paykids/GlobalApplication.kt
+++ PayKids/app/src/main/java/com/paykids/paykids/GlobalApplication.kt
@@ -1,6 +1,7 @@
 package com.paykids.paykids
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import com.paykids.util.LoggerUtils
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,7 +9,8 @@ import dagger.hilt.android.HiltAndroidApp
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
+//        Log.d("KeyHash", Utility.getKeyHash(this))
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
         printStartingLog()
     }
 
--- PayKids/app/src/main/java/com/paykids/paykids/MainActivity.kt
+++ PayKids/app/src/main/java/com/paykids/paykids/MainActivity.kt
@@ -1,24 +1,56 @@
 package com.paykids.paykids
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kakao.sdk.common.KakaoSdk
-import com.paykids.presentation.view.splash.SplashActivity
+import com.paykids.paykids.databinding.ActivityMainBinding
+import com.paykids.presentation.view.signIn.SignActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var splashScreen: SplashScreen
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
-//        Log.d("KeyHash", Utility.getKeyHash(this))
-        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val intent = Intent(this, SplashActivity::class.java)
+//        startSplash()
+
+        val intent = Intent(this, SignActivity::class.java)
         startActivity(intent)
+    }
 
-        finish()
+    private fun startSplash() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 5f, 1f)
+            val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 5f, 1f)
+
+            ObjectAnimator.ofPropertyValuesHolder(splashScreenView.iconView, scaleX, scaleY).run {
+                interpolator = AnticipateInterpolator()
+                duration = 1000L
+                doOnEnd {
+                    splashScreenView.remove()
+
+                }
+                start()
+            }
+        }
     }
 }
