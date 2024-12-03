164a0ac4570c4012a0011ccdf1f97799b944a59d
HamBeomJoon <hbj0802@naver.com>
2024-12-03 14:09:41 +0900

    feat: 용돈일기 페이지 임시 생성

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
@@ -0,0 +1,8 @@
+package com.paykids.presentation.view.diary
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DiaryFragment: Fragment() {
+}
