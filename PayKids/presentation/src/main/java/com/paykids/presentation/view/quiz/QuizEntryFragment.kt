8e8a114035fbf9c6b7ebc5453e37fbc716a6d0c9
HamBeomJoon <hbj0802@naver.com>
2024-12-04 12:13:38 +0900

    feat: 퀴즈 진입 페이지 추가

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/quiz/QuizEntryFragment.kt
@@ -0,0 +1,12 @@
+package com.paykids.presentation.view.quiz
+
+import com.paykids.presentation.base.BaseFragment
+import com.paykids.presentation.databinding.FragmentMypageBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class QuizEntryFragment: BaseFragment<FragmentMypageBinding>() {
+    override fun initView() {
+        
+    }
+}
