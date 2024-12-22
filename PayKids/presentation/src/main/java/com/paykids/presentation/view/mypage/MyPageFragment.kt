67115cc1402f52769dc9879805e428d081116560
HamBeomJoon <hbj0802@naver.com>
2024-12-22 18:00:23 +0900

    feat: 마이페이지에서 약관페이지, 수정페이지로 이동

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/mypage/MyPageFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/mypage/MyPageFragment.kt
@@ -2,6 +2,7 @@ package com.paykids.presentation.view.mypage
 
 import android.content.Intent
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.paykids.presentation.R
 import com.paykids.presentation.base.BaseFragment
 import com.paykids.presentation.databinding.FragmentMypageBinding
@@ -20,19 +21,15 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>() {
 
     override fun initListener() {
         super.initListener()
+        val navController = findNavController()
+
 
         binding.ivModify.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fl_home, ModifyInfoFragment())
-                .addToBackStack(null)
-                .commit()
+            navController.navigate(R.id.modifyInfoFragment)
         }
 
         binding.ivPolicy.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fl_home, PolicyFragment())
-                .addToBackStack(null)
-                .commit()
+            navController.navigate(R.id.policyFragment)
         }
 
         binding.btnLogout.setOnClickListener {
