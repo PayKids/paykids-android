ea870e47dbc8460efefe17900bdc1054ffe7e951
HamBeomJoon <hbj0802@naver.com>
2024-12-23 02:11:02 +0900

    feat: 용돈일기 페이지 화면 추가 (달력 제외)

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
@@ -1,8 +1,14 @@
 package com.paykids.presentation.view.diary
 
 import androidx.fragment.app.Fragment
+import com.paykids.presentation.base.BaseFragment
+import com.paykids.presentation.databinding.FragmentDiaryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DiaryFragment: Fragment() {
+class DiaryFragment : BaseFragment<FragmentDiaryBinding>() {
+
+    override fun initView() {
+    }
+
 }
