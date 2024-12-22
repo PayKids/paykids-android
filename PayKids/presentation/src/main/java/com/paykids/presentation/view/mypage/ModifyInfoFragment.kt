8cf0b97edb4767315b36d0f6a312e54d4479944c
HamBeomJoon <hbj0802@naver.com>
2024-12-22 19:13:32 +0900

    feat: 내 정보 수정 - 프로필 사진 변경 기능 추가

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/mypage/ModifyInfoFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/mypage/ModifyInfoFragment.kt
@@ -1,12 +1,14 @@
 package com.paykids.presentation.view.mypage
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.paykids.presentation.base.BaseFragment
 import com.paykids.presentation.databinding.FragmentModifyInfoBinding
-import com.paykids.presentation.databinding.FragmentMypageBinding
 import com.paykids.presentation.utils.UiState
-import com.paykids.presentation.view.home.HomeActivity
 import com.paykids.presentation.view.signIn.SignActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,6 +24,10 @@ class ModifyInfoFragment : BaseFragment<FragmentModifyInfoBinding>() {
 
         binding.ibBack.setOnClickListener {
             parentFragmentManager.popBackStack()
+        }
+
+        binding.ivModifyImage.setOnClickListener {
+            openGallery()
         }
 
         binding.tvWithdraw.setOnClickListener {
@@ -61,5 +67,20 @@ class ModifyInfoFragment : BaseFragment<FragmentModifyInfoBinding>() {
                 }
             }
         }
+    }
+
+    private val galleryLauncher =
+        this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val selectedImageUri: Uri? = result.data?.data
+                selectedImageUri?.let {
+                    binding.ivProfile.setImageURI(it)
+                }
+            }
+        }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(intent)
     }
 }
