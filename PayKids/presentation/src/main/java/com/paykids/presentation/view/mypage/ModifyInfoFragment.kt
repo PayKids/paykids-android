393aaa79c6a3b7577972477a4cb17e82ef68173f
HamBeomJoon <hbj0802@naver.com>
2024-12-22 17:45:31 +0900

    feat: 회원 정보 수정 페이지 추가

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/mypage/ModifyInfoFragment.kt
@@ -0,0 +1,61 @@
+package com.paykids.presentation.view.mypage
+
+import android.content.Intent
+import androidx.fragment.app.viewModels
+import com.paykids.presentation.base.BaseFragment
+import com.paykids.presentation.databinding.FragmentModifyInfoBinding
+import com.paykids.presentation.databinding.FragmentMypageBinding
+import com.paykids.presentation.utils.UiState
+import com.paykids.presentation.view.home.HomeActivity
+import com.paykids.presentation.view.signIn.SignActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ModifyInfoFragment : BaseFragment<FragmentModifyInfoBinding>() {
+    private val myPageViewModel: MyPageViewModel by viewModels()
+
+    override fun initView() {
+    }
+
+    override fun initListener() {
+        super.initListener()
+
+        binding.tvWithdraw.setOnClickListener {
+            myPageViewModel.withdraw()
+        }
+    }
+
+    override fun setObserver() {
+        super.setObserver()
+
+        myPageViewModel.withdrawState.observe(viewLifecycleOwner) {
+            when (it) {
+                is UiState.Loading -> {}
+                is UiState.Failure -> {
+                    showToast("회원 탈퇴 실패")
+                }
+
+                is UiState.Success -> {
+                    myPageViewModel.clearData()
+                }
+            }
+        }
+
+        myPageViewModel.clearState.observe(viewLifecycleOwner)
+        {
+            when (it) {
+                is UiState.Loading -> {}
+                is UiState.Failure -> {
+                    showToast("회원 정보 삭제 실패")
+                }
+
+                is UiState.Success -> {
+                    requireActivity().apply {
+                        startActivity(Intent(this, SignActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+}
