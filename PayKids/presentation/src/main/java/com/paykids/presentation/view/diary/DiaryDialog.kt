02f254610a6fe62c9e82d989462a87211eb6e783
HamBeomJoon <hbj0802@naver.com>
2024-12-23 19:16:24 +0900

    style: remove unused import

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryDialog.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryDialog.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
 import com.paykids.presentation.R
 import com.paykids.presentation.custom.ConfirmDialogInterface
