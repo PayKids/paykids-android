4c6c3eb34cfc2d89afb5bdbd66b36059c478daea
HamBeomJoon <hbj0802@naver.com>
2024-12-23 19:14:29 +0900

    feat: 상세 소비내역 dummy data 추가

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
@@ -1,13 +1,22 @@
 package com.paykids.presentation.view.diary
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.paykids.domain.model.DetailConsume
 import com.paykids.presentation.base.BaseFragment
 import com.paykids.presentation.custom.ConfirmDialogInterface
-import com.paykids.presentation.custom.DiaryDialog
 import com.paykids.presentation.databinding.FragmentDiaryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DiaryFragment : BaseFragment<FragmentDiaryBinding>(), ConfirmDialogInterface {
+
+    private val items = arrayListOf(
+        DetailConsume("편의점", -1800, "메모1"),
+        DetailConsume("편의점", -1800, "메모2"),
+        DetailConsume("편의점", -1800, "메모3"),
+        DetailConsume("편의점", -1800, "메모4")
+    )
 
     override fun initView() {
     }
@@ -19,6 +28,12 @@ class DiaryFragment : BaseFragment<FragmentDiaryBinding>(), ConfirmDialogInterfa
             val dialog = DiaryDialog(this)
             dialog.isCancelable = true
             dialog.show(parentFragmentManager, "AddPocketMoneyDialog")
+        }
+
+        val adapter = DetailConsumeAdapter(items)
+        binding.rvDetailConsume.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            this.adapter = adapter
         }
     }
 
