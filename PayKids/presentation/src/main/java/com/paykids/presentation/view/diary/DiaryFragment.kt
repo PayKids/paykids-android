7ef1faaae33c3c0514f73aba986eaf8daa42c044
HamBeomJoon <hbj0802@naver.com>
2024-12-23 16:50:52 +0900

    feat: 용돈 기입 다이얼로그 호출

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/diary/DiaryFragment.kt
@@ -1,14 +1,29 @@
 package com.paykids.presentation.view.diary
 
-import androidx.fragment.app.Fragment
 import com.paykids.presentation.base.BaseFragment
+import com.paykids.presentation.custom.ConfirmDialogInterface
+import com.paykids.presentation.custom.DiaryDialog
 import com.paykids.presentation.databinding.FragmentDiaryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DiaryFragment : BaseFragment<FragmentDiaryBinding>() {
+class DiaryFragment : BaseFragment<FragmentDiaryBinding>(), ConfirmDialogInterface {
 
     override fun initView() {
+    }
+
+    override fun initListener() {
+        super.initListener()
+
+        binding.ibAddPocketMoney.setOnClickListener {
+            val dialog = DiaryDialog(this)
+            dialog.isCancelable = true
+            dialog.show(parentFragmentManager, "AddPocketMoneyDialog")
+        }
+    }
+
+    override fun onYesButtonClick() {
+
     }
 
 }
