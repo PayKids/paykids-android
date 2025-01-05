42f6d9b4c12fa8a350f91de19d58f8b276fcba11
HamBeomJoon <hbj0802@naver.com>
2025-01-06 00:23:29 +0900

    feat: 임시로 로그인 생략

--- PayKids/domain/src/main/java/com/paykids/domain/usecase/auth/SaveSignInInfoUseCase.kt
+++ PayKids/domain/src/main/java/com/paykids/domain/usecase/auth/SaveSignInInfoUseCase.kt
@@ -8,6 +8,8 @@ class SaveSignInInfoUseCase(private val repository: DataStoreRepository) {
         val step1 = repository.setAccessToken(accessToken).isSuccess
         val step2 = repository.setRefreshToken(refreshToken).isSuccess
 
-        return if(step1 && step2) Result.success(true) else Result.success(false)
+        return if (step1 && step2) Result.success(true) else {
+            Result.failure(IllegalStateException("AccessToken/RefreshToken 저장 실패"))
+        }
     }
 }
--- PayKids/presentation/src/main/java/com/paykids/presentation/view/signIn/SignInProviderFragment.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/signIn/SignInProviderFragment.kt
@@ -19,7 +19,8 @@ class SignInProviderFragment : BaseFragment<FragmentSignProviderBinding>() {
         super.initListener()
 
         binding.btnKakao.setOnClickListener {
-            signViewModel.signInWithKakao()
+//            signViewModel.signInWithKakao()
+            navigateToHome()
         }
     }
 
