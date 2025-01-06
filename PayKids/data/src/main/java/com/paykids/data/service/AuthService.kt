85589c45eb7cff1c20ec24386675b471da4873b6
HamBeomJoon <hbj0802@naver.com>
2025-01-07 02:17:08 +0900

    fix: 로그인 api관련 Header 변경

--- PayKids/data/src/main/java/com/paykids/data/service/AuthService.kt
+++ PayKids/data/src/main/java/com/paykids/data/service/AuthService.kt
@@ -3,6 +3,7 @@ package com.paykids.data.service
 import com.paykids.data.model.BaseResponse
 import com.paykids.data.model.UserTokenResponseDTO
 import retrofit2.Response
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -10,12 +11,12 @@ interface AuthService {
 
     @POST("/auth/login")
     suspend fun signIn(
-        @Query("idToken") idToken: String,
+        @Header("Authorization") idToken: String,
     ): Response<BaseResponse<UserTokenResponseDTO>>
 
     @POST("/auth/refresh")
     suspend fun getRefreshToken(
-        @Query("refreshToken") refreshToken: String,
+        @Header("Authorization") refreshToken: String,
     ): Response<BaseResponse<UserTokenResponseDTO>>
 
 }
