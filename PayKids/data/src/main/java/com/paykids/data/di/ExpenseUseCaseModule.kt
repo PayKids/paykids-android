c05d421865e6d2f173b14df94ba067a84ac288ea
HamBeomJoon <hbj0802@naver.com>
2025-01-07 13:53:18 +0900

    feat: Expense Usecase DI

--- PayKids/data/src/main/java/com/paykids/data/di/ExpenseUseCaseModule.kt
+++ PayKids/data/src/main/java/com/paykids/data/di/ExpenseUseCaseModule.kt
@@ -2,7 +2,10 @@ package com.paykids.data.di
 
 import com.paykids.domain.repository.ExpenseRepository
 import com.paykids.domain.repository.UserRepository
+import com.paykids.domain.usecase.expense.GetDayExpenseUseCase
+import com.paykids.domain.usecase.expense.GetMonthAllCategoryUseCase
 import com.paykids.domain.usecase.expense.GetMonthDailyExpenseUseCase
+import com.paykids.domain.usecase.expense.GetMonthMostCategoryUseCase
 import com.paykids.domain.usecase.expense.GetMonthTotalExpenseUseCase
 import com.paykids.domain.usecase.user.ChangeNicknameUseCase
 import com.paykids.domain.usecase.user.GetUserInfoUseCase
@@ -28,10 +31,42 @@ object ExpenseUseCaseModule {
 
     @Provides
     @Singleton
-    fun provideGetMonthDailyExpenseUseCaseUseCase(
+    fun provideGetMonthMostCategoryUseCase(
+        repository: ExpenseRepository
+    ): GetMonthMostCategoryUseCase {
+        return GetMonthMostCategoryUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMonthDailyExpenseUseCase(
         repository: ExpenseRepository
     ): GetMonthDailyExpenseUseCase {
         return GetMonthDailyExpenseUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMonthCategoryExpenseUseCase(
+        repository: ExpenseRepository
+    ): GetMonthMostCategoryUseCase {
+        return GetMonthMostCategoryUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMonthAllCategoryUseCase(
+        repository: ExpenseRepository
+    ): GetMonthAllCategoryUseCase {
+        return GetMonthAllCategoryUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetDayExpenseUseCase(
+        repository: ExpenseRepository
+    ): GetDayExpenseUseCase {
+        return GetDayExpenseUseCase(repository)
     }
 
 }
