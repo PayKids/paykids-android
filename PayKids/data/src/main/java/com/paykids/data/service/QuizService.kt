cdb66b75469c50501529f84b78b9d84f41dec6a5
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-09 00:07:18 +0900

    feat: Quiz Service 생성

---
+++ PayKids/data/src/main/java/com/paykids/data/service/QuizService.kt
@@ -0,0 +1,21 @@
+package com.paykids.data.service
+
+import com.paykids.data.model.BaseResponse
+import com.paykids.data.model.QuizResponseDTO
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface QuizService {
+    @GET("/quiz")
+    suspend fun getQuiz(
+        @Query("stage") stage: Int,
+        @Query("number") number: Int
+    ): Response<BaseResponse<QuizResponseDTO>>
+
+    @GET("/quiz/stage-name")
+    suspend fun getStageName(
+        @Query("stage") stage: Int
+    ): Response<BaseResponse<String>>
+}
