c2cc634378767ed51444af485ae76b287029a5ae
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-09 00:09:04 +0900

    feat: Quiz Repository 생성

---
+++ PayKids/domain/src/main/java/com/paykids/domain/repository/QuizRepository.kt
@@ -0,0 +1,9 @@
+package com.paykids.domain.repository
+
+import com.paykids.domain.model.quiz.Quiz
+
+interface QuizRepository {
+    suspend fun getQuiz(stage: Int, number: Int) : Result<Quiz>
+
+    suspend fun getStageName(stage: Int) : Result<String>
+}
