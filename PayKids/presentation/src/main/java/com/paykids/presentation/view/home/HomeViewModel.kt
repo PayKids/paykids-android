2fc6107ac7ced84741a773adf8b369c1a5518d89
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-09 22:59:04 +0900

    feat: viewModel 해금된 스테이지 번호 조회 함수 추가

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/home/HomeViewModel.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/home/HomeViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.paykids.domain.usecase.datastore.GetAccessTokenUseCase
 import com.paykids.domain.usecase.quiz.GetStageCountUseCase
 import com.paykids.domain.usecase.quiz.GetStageNameUseCase
+import com.paykids.domain.usecase.quiz.GetStageToGoUseCase
 import com.paykids.presentation.utils.UiState
 import com.paykids.util.LoggerUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,8 +16,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
+    private val getAccessTokenUseCase: GetAccessTokenUseCase,
     private val getStageNameUseCase: GetStageNameUseCase,
-    private val getStageCountUseCase: GetStageCountUseCase
+    private val getStageCountUseCase: GetStageCountUseCase,
+    private val getStageToGoUseCase: GetStageToGoUseCase
 ) : ViewModel() {
 
     private val _stageNameState = MutableLiveData<UiState<String>>(UiState.Loading)
@@ -54,6 +58,27 @@ class HomeViewModel @Inject constructor(
                 .onFailure {
                     _stageNameState.value = UiState.Failure(message = "스테이지 개수 조회 실패")
                     LoggerUtils.e("스테이지 개수 조회 실패")
+                }
+        }
+    }
+
+    private val _stageToGoState = MutableLiveData<UiState<Int>>(UiState.Loading)
+    val stageToGoState: LiveData<UiState<Int>> get() = _stageToGoState
+
+    fun getStageToGo() {
+        _stageToGoState.value = UiState.Loading
+
+        viewModelScope.launch {
+            //getStageToGoUseCase.invoke("eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiNjRjMjk5OTktMmIxYS00NDUwLWEzNWMtMzNiN2NmZTBiZTlhIiwiZW1haWwiOiJwYXlraWRzMjAyNEBnbWFpbC5jb20iLCJpYXQiOjE3MzY0MDQ2NTcsImV4cCI6MTczNjQ0Nzg1N30.-UpYS1ZEloR_JHDKDj0fdeQ57PDJ4IaxoDTeIiEtcR8")
+            getStageToGoUseCase.invoke(getAccessTokenUseCase.invoke().getOrNull().toString())
+                .onSuccess {
+                    _stageToGoState.value =
+                        UiState.Success(it)
+                    LoggerUtils.d("진행할 스테이지 번호 조회 성공: $it")
+                }
+                .onFailure {
+                    _stageToGoState.value = UiState.Failure(message = "진행할 스테이지 번호 조회 실패")
+                    LoggerUtils.e("진행할 스테이지 번호 조회 실패")
                 }
         }
     }
