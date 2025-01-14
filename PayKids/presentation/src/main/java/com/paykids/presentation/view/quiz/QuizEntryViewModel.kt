2b1faab350d64a8fea75e603db2a5a0172e9d3f7
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-14 22:38:50 +0900

    feat: viewModel 스테이지 클리어 여부 조회 함수 추가

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/quiz/QuizEntryViewModel.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/quiz/QuizEntryViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.paykids.domain.model.quiz.Quiz
+import com.paykids.domain.model.quiz.QuizClear
 import com.paykids.domain.usecase.datastore.GetAccessTokenUseCase
 import com.paykids.domain.usecase.quiz.CheckAnswerUseCase
+import com.paykids.domain.usecase.quiz.CheckClearUseCase
 import com.paykids.domain.usecase.quiz.GetQuizUseCase
 import com.paykids.presentation.utils.UiState
 import com.paykids.util.LoggerUtils
@@ -18,7 +20,8 @@ import javax.inject.Inject
 class QuizEntryViewModel @Inject constructor(
     private val getAccessTokenUseCase: GetAccessTokenUseCase,
     private val getQuizUseCase: GetQuizUseCase,
-    private val checkAnswerUseCase: CheckAnswerUseCase
+    private val checkAnswerUseCase: CheckAnswerUseCase,
+    private val checkClearUseCase: CheckClearUseCase
 ) : ViewModel() {
     private val _quizState = MutableLiveData<UiState<Quiz>>(UiState.Loading)
     val quizState: LiveData<UiState<Quiz>> get() = _quizState
@@ -27,24 +30,21 @@ class QuizEntryViewModel @Inject constructor(
         _quizState.value = UiState.Loading
 
         viewModelScope.launch {
-            getQuizUseCase.invoke(stage, number)
-                .onSuccess {
-                    _quizState.value =
-                        UiState.Success(
-                            Quiz(
-                                it.answer,
-                                it.choices,
-                                it.count,
-                                it.id,
-                                it.imageURL,
-                                it.number,
-                                it.question,
-                                it.quizType,
-                                it.stage
-                            )
+            getQuizUseCase.invoke(stage, number).onSuccess {
+                    _quizState.value = UiState.Success(
+                        Quiz(
+                            it.answer,
+                            it.choices,
+                            it.count,
+                            it.id,
+                            it.imageURL,
+                            it.number,
+                            it.question,
+                            it.quizType,
+                            it.stage
                         )
-                }
-                .onFailure {
+                    )
+                }.onFailure {
                     _quizState.value = UiState.Failure(message = "퀴즈 불러오기 실패")
                 }
         }
@@ -57,25 +57,45 @@ class QuizEntryViewModel @Inject constructor(
         _checkAnswerState.value = UiState.Loading
 
         viewModelScope.launch {
-//            checkAnswerUseCase.invoke(
-//                "eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiNjRjMjk5OTktMmIxYS00NDUwLWEzNWMtMzNiN2NmZTBiZTlhIiwiZW1haWwiOiJwYXlraWRzMjAyNEBnbWFpbC5jb20iLCJpYXQiOjE3MzY3NzUzNDksImV4cCI6MTczNjgxODU0OX0.rUYfMx5s7FjxbzOdMdm_ZpRiLPlj3e8uqvsyWI-d7YY",
-//                stage,
-//                number,
-//                answer
-//            )
             checkAnswerUseCase.invoke(
-                getAccessTokenUseCase.invoke().getOrNull().toString(),
+                "eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiNjRjMjk5OTktMmIxYS00NDUwLWEzNWMtMzNiN2NmZTBiZTlhIiwiZW1haWwiOiJwYXlraWRzMjAyNEBnbWFpbC5jb20iLCJpYXQiOjE3MzY4MjAzNzUsImV4cCI6MTczNjg2MzU3NX0.1exX260QmpUlOfoFRgM55K5f_NTeMclk_oiEaECxLv0",
                 stage,
                 number,
                 answer
             )
-                .onSuccess {
+//            checkAnswerUseCase.invoke(
+//                getAccessTokenUseCase.invoke().getOrNull().toString(), stage, number, answer
+//            )
+            .onSuccess {
                     _checkAnswerState.value = UiState.Success(it)
                     LoggerUtils.d("답 확인 성공: $it")
-                }
-                .onFailure {
+                }.onFailure {
                     _checkAnswerState.value = UiState.Failure(message = "답 확인 실패")
                     LoggerUtils.e("답 확인 실패")
+                }
+        }
+    }
+
+    private val _checkClearState = MutableLiveData<UiState<QuizClear>>(UiState.Loading)
+    val checkClearState: LiveData<UiState<QuizClear>> get() = _checkClearState
+
+    fun checkClear(stage: Int) {
+        _checkClearState.value = UiState.Loading
+
+        viewModelScope.launch {
+//            checkClearUseCase.invoke(
+//                "eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiNjRjMjk5OTktMmIxYS00NDUwLWEzNWMtMzNiN2NmZTBiZTlhIiwiZW1haWwiOiJwYXlraWRzMjAyNEBnbWFpbC5jb20iLCJpYXQiOjE3MzY4MjAzNzUsImV4cCI6MTczNjg2MzU3NX0.1exX260QmpUlOfoFRgM55K5f_NTeMclk_oiEaECxLv0",
+//                stage
+//            )
+            checkClearUseCase.invoke(
+                getAccessTokenUseCase.invoke().getOrNull().toString(), stage
+            )
+            .onSuccess {
+                    _checkClearState.value = UiState.Success(it)
+                    LoggerUtils.d("클리어 확인 성공: $it")
+                }.onFailure {
+                    _checkClearState.value = UiState.Failure(message = "클리어 확인 실패")
+                    LoggerUtils.e("클리어 확인 실패")
                 }
         }
     }
