5ae0ff0018acfdd0a8b9114e36c232886582ab4a
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-15 03:12:30 +0900

    refactor: import refactor

--- PayKids/presentation/src/main/java/com/paykids/presentation/view/quiz/QuizMultipleChoiceRvAdapter.kt
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/quiz/QuizMultipleChoiceRvAdapter.kt
@@ -4,8 +4,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.paykids.presentation.R
 
