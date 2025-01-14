c28ca58f9bf8cdf70120b68de44276d18f02437c
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-01-14 22:37:40 +0900

    feat: CheckClearUseCase 작성

---
+++ PayKids/domain/src/main/java/com/paykids/domain/usecase/quiz/CheckClearUseCase.kt
@@ -0,0 +1,11 @@
+package com.paykids.domain.usecase.quiz
+
+import com.paykids.domain.model.quiz.QuizClear
+import com.paykids.domain.repository.QuizRepository
+import javax.inject.Inject
+
+class CheckClearUseCase @Inject constructor(private val repository: QuizRepository) {
+    suspend operator fun invoke(accessToken: String, stage: Int): Result<QuizClear> {
+        return repository.checkClear(accessToken, stage)
+    }
+}
