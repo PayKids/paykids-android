108431d4aa47c24d1c4a73f68bec194a5f1e77f9
HamBeomJoon <hbj0802@naver.com>
2025-01-20 01:33:11 +0900

    feat: 퀘스트 화면 연결

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/quest/QuestFragment.kt
@@ -0,0 +1,44 @@
+package com.paykids.presentation.view.quest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
+import com.paykids.presentation.databinding.FragmentQuestBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class QuestFragment : Fragment() {
+    private var _binding: FragmentQuestBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQuestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = QuestPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "퀘스트"
+                1 -> "업적"
+                else -> null
+            }
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
