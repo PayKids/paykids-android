49bedb3b10feda79757d69d41548acafa311830f
HamBeomJoon <hbj0802@naver.com>
2025-01-20 01:33:24 +0900

    feat: 퀘스트 화면 퀘스트, 업적 탭 추가

---
+++ PayKids/presentation/src/main/java/com/paykids/presentation/view/quest/QuestListFragment.kt
@@ -0,0 +1,19 @@
+package com.paykids.presentation.view.quest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.paykids.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class QuestListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_quest_list, container, false)
+    }
+}
