1e62f335a2a0c48e2a251c8010de7ff40a0ae600
HamBeomJoon <hbj0802@naver.com>
2025-02-02 22:26:59 +0900

    feat: 업적 조회 dataSource, dataSourceImpl

---
+++ PayKids/data/src/main/java/com/paykids/data/datasourceImpl/AchievementRemoteDatasourceImpl.kt
@@ -0,0 +1,32 @@
+package com.paykids.data.datasourceImpl
+
+import com.paykids.data.datasource.AchievementRemoteDatasource
+import com.paykids.data.datasource.ChatRemoteDatasource
+import com.paykids.data.model.ChatResponseDTO
+import com.paykids.data.model.achievement.AchieveResponseDTO
+import com.paykids.data.service.AchievementService
+import com.paykids.data.service.ChatService
+import javax.inject.Inject
+
+class AchievementRemoteDatasourceImpl @Inject constructor(
+    private val achievementService: AchievementService
+) : AchievementRemoteDatasource {
+    override suspend fun getAchievements(accessToken: String): Result<AchieveResponseDTO> {
+        return try {
+            val response = achievementService.getAchievements(accessToken)
+            if (response.isSuccessful) {
+                val chatResponse = response.body()
+
+                if (chatResponse != null) {
+                    Result.success(chatResponse)
+                } else {
+                    Result.failure(Exception("get Achievements failed: response body is null"))
+                }
+            } else {
+                Result.failure(Exception("get Achievements failed: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
