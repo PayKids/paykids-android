11e1822f1cd2461f7ed5f5c90702c5120c9ff302
HamBeomJoon <hbj0802@naver.com>
2025-02-06 23:43:57 +0900

    deploy: 1.0.3버전 배포

--- PayKids/app/build.gradle.kts
+++ PayKids/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.paykids.paykids"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10002
-        versionName = "1.00.02"
+        versionCode = 103
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
